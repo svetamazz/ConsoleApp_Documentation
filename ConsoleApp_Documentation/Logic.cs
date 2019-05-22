7f0571837ca3d579ca2061958a6d53515ffb2af6
ghabes <swetamazkiw@gmail.com>
2019-05-22 19:17:22 +0300

    Add project files.

---
+++ ConsoleApp_Documentation/Logic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Documentation
+{
+    /// <summary>
+    /// My Logic Class with some methods
+    /// </summary>
+    public class Logic
+    {
+        /// <summary>
+        /// This is my property
+        /// </summary>
+        public int MyProperty { get; set; }
+
+        /// <summary>
+        /// This method check param for current year
+        /// <para> Second Paragraph</para>
+        /// <para> Last Paragraph</para>
+        /// </summary>
+        /// <param name="year">Integer value for year</param>
+        /// <param name="month">Integer value for month</param>
+        /// <exception cref="ArgumentOutOfRangeException">Якщо дані під рік та місяць від'ємні</exception>
+        /// <seealso cref="Console"/>
+        /// <returns>True if year is year is current, False if not</returns>
+        public bool checkYear(int year,int month)
+        {
+            return year == DateTime.Now.Year && month==DateTime.Now.Month;
+        }
+        /// <summary>
+        /// This method performs division
+        /// </summary>
+        /// <param name="x">Integer value for x(divider)</param>
+        /// <param name="y">Integer value for y(dividend)</param>
+        /// <exception cref="ArgumentException">If y(dividend) = 0</exception>
+        /// <returns>Result of division</returns>
+        public double division(int x,int y)
+        {
+            if (y == 0) throw new ArgumentException("Can't divite on 0.");
+
+            return x / y;
+        }
+    }
+}
---
+++ ConsoleApp_Documentation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Documentation
+{
+    /// <summary>
+    /// Main Class Of Application
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starting Point
+        /// </summary>
+        /// <param name="args">Console parameters</param>
+        static void Main(string[] args)
+        {
+           var logic = new Logic();
+            bool res = logic.checkYear(2000, 3);
+            
+        }
+    }
+}
